5d7b8417c23e66ae109be80266473a014e4c7a75
Minotto01 <gabriel.minotto5@gmail.com>
2023-03-26 18:21:06 -0300

    ajustes no corpo do código

--- main.py
+++ main.py
@@ -1,52 +1,32 @@
-''' 
-  TODO funções:
-  # nota: o indice da linha que tem o valor ZERO é a cidade daquela linha
-  # linha é uma lista
-  # criar def caminhão
-  # criar def menu
-  # TRATAMENTO DE STRING nos inputs das cidades
-    # Real com vírgula
-    # nome em maísculo ou minúsculo, espaços, acentos, 
-  # chamar os menus() dentro das match cases de resposta
-    
-  
-  TODO implementações:
-  1 [Consultar trechos x modalidade]
-        -INPUT: nome de duas cidades e a modalidade(porte);
-        -OUTPUT: distancia, custo total, se cidade não existir avisar;
-        -EXEMPLO: (de PORTO ALEGRE para SÃO PAULO, utilizando um caminhão 
-                   de pequeno porte, a distância é de XXX km e o custo 
-                   será de R$ xxx,00.)
-
-  2 [Cadastrar transporte, itens e pesos]
-        -INPUT: uma sequência de cidades, itens e pesos
-        -OUTPUT: distancia total, modelo mais custo-benefício e custos
-        -EXEMPLO: (de PORTO ALEGRE para SÃO PAULO, a  distância 
-            a ser percorrida é de X km, para transporte dos produtos X, Y , Z 
-            será necessário utilizar 2 caminhões de porte PEQUENO e um de porte 
-            MÉDIO, de forma a resultar no menor custo de transporte por km rodado. 
-            O valor total do transporte dos itens é R$ xxx,00, sendo R$ xxx,00 é o 
-            custo unitário médio.)
-
-  3 [Dados estatísticos]
-        -OUTPUT: exibir um relatório dos transportes cadastrados, para cada transporte 
-        apresentar: CUSTO TOTAL; CUSTO POR TRECHO; CUSTO MÉDIO POR KM(TOTAL);
-        CUSTO MÉDIO POR PRODUTO(CUSTO POR QUANTIDADE?); CUSTO TOTAL POR TRECHO; CUSTO TOTAL
-        PARA CADA MODALIDADE; TOTAL DE VEÍCULOS; TOTAL DE ITENS, 
-        se não ouver cadastros avisar
-        # menu_transportes_cadastrados()  lista de informações 
-'''
-
-
-
-
-### ÁREA DO MENU VISUAL ###
-
-from menus import *
+from menu import * 
 import pandas as pd # Para manipulação de dados do arquivo CSV nas funções do sistema
 
 
 distancias = pd.read_csv('DNIT-distancias.csv', sep=';') # Lê o arquivo csv
 
-menu_principal = menu(['Consultar um Transporte', 'Cadastrar um Transporte', 'Registro de Cadastros', 'Encerrar Sessão'])
-print(menu_principal)
+while True:
+    menu_principal = menu(['Consultar um Transporte', 'Cadastrar um Transporte', 
+                           'Registro de Cadastros', 'Encerrar Sessão'], 'MENU PRINCIPAL')
+    if menu_principal == 1:
+        cabeçalho('Opção 1')
+    elif menu_principal == 2:
+        cabeçalho('Opção 2')
+    elif menu_principal == 3:
+        cabeçalho('Opção 3')
+    elif menu_principal == 4:
+        confirm = input('Tem certeza que deseja sair? (S/N) ').lower()
+        while confirm != 's' or 'n':
+            separador()
+            confirm = input('Não entendi, por favor digite (S/N): ').lower()
+            if confirm == 's':
+                break
+            elif confirm == 'n':
+                break
+        if confirm == 's':
+                cabeçalho('Que pena! Até logo, volte sempre!')
+                break
+        elif confirm == 'n':
+                continue
+    else:
+        print('Digite uma opção existente.')
+
--- menu.py
+++ menu.py
@@ -2,37 +2,36 @@ def separador(tamanho = 42): # cria uma função de rodapé visual
     return '=' * tamanho
 
 
-def cabeçalho(titulo): # cria uma função de cabeçalho visual com título
+def cabeçalho(titulo): # cria uma função de cabeçalho visual para título
     print(separador())
     print(titulo.center(42))
     print(separador())
 
 
-def verif_int(msg):
+def verif_int(resposta):
   while True: 
       try:
-          n = int(input(msg))
+          n = int(input(resposta))
       except(ValueError, TypeError):
-            print("Por favor, digite uma opção válida.")
+            cabeçalho("Por favor, digite uma opção válida.")
             continue
       else:
          return n
 
 
-def menu(lista):
-    cabeçalho('MENU PRINCIPAL')
-    opc_do_menu = 1
+def menu(lista, titulo):
+    cabeçalho(titulo)
+    num = 1
     for item in lista:
-        print(f'[{opc_do_menu}] - {item}')
-        opc_do_menu += 1
+        print(f'[{num}] - {item}')
+        num += 1
     print(separador())
     opc_do_usuario = verif_int('Digite uma opção: ')
     return opc_do_usuario
 
 
-
-
-
+ 
+           
 
 
 
