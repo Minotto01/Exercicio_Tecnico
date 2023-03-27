94c4c97b9d671e028a9eb7ffcf5d5727cee0f59b
Minotto01 <gabriel.minotto5@gmail.com>
2023-03-26 22:21:04 -0300

    fix: correções de funcionalidade geral

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import pandas as pd # Para manipulação de dados do arquivo CSV nas funções d
 
 distancias = pd.read_csv('DNIT-distancias.csv', sep=';') # Lê o arquivo csv
 
+registro = 'tabela_registro.txt'
+if not verificar_registro(registro):
+     criar_registro(registro)
+
+
+                    
+
 while True:
     menu_principal = menu(['Consultar um Transporte', 'Cadastrar um Transporte', 
                            'Registro de Cadastros', 'Encerrar Sessão'], 'MENU PRINCIPAL')
@@ -15,18 +22,18 @@ while True:
         cabeçalho('Opção 3')
     elif menu_principal == 4:
         confirm = input('Tem certeza que deseja sair? (S/N) ').lower()
-        while confirm != 's' or 'n':
-            separador()
-            confirm = input('Não entendi, por favor digite (S/N): ').lower()
-            if confirm == 's':
-                break
-            elif confirm == 'n':
-                break
-        if confirm == 's':
+        match confirm:
+            case 's':
                 cabeçalho('Que pena! Até logo, volte sempre!')
-                break
-        elif confirm == 'n':
+            case 'n':
                 continue
+            case outro:
+                while confirm == 's' or 'n':
+                    confirm = input('Não entendi, por favor digite (S/N): ')
+                    match confirm:
+                        case 's':
+                            cabeçalho('Que pena! Até logo, volte sempre!')
+                        case 'n':
+                            break
     else:
         print('Digite uma opção existente.')
-
--- modulos_menu.py
+++ modulos_menu.py
@@ -30,7 +30,36 @@ def menu(lista, titulo):
     return opc_do_usuario
 
 
- 
+
+def verificar_registro(registro):
+    try:
+        abrir = open(registro, 'rt')
+        abrir.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def criar_registro(registro):
+    try:
+        abrir = open(registro, 'wt+')
+        abrir.close()
+    except:
+        print('Não foi possível criar um registro')
+    else:
+        print('Registro criado')
+
+def ler_registro(registro):
+    try:
+        abrir = open(registro, 'rt')
+        abrir.close()
+    except:
+        print('Não foi possível ler a tabela')
+    else:
+        cabeçalho('Registro de cadastros')
+        print(abrir.read())
+
+
            
 
 
